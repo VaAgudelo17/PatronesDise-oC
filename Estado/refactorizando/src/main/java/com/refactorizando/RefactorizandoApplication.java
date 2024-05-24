dccbbc7e11765816861264a5b030a7fab2c1b5c4
valentina <valeagu1710@gmail.com>
2024-05-24 16:56:21 -0500

    state pattern created

---
+++ Estado/refactorizando/src/main/java/com/refactorizando/RefactorizandoApplication.java
@@ -0,0 +1,13 @@
+package com.refactorizando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactorizandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactorizandoApplication.class, args);
+	}
+
+}
