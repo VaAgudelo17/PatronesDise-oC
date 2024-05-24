e5dd85da764437e68a7fbe4ae57dc8679c7e05b9
VaAgudelo17 <114451968+VaAgudelo17@users.noreply.github.com>
2024-05-24 09:00:01 -0500

    Decorator pattern created

--- Estructurales/decorador/src/main/java/com/formacion/decorador/Main.java
+++ Estructurales/decorador/src/main/java/com/formacion/decorador/Main.java
@@ -2,7 +2,7 @@ package com.formacion.decorador;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Holii uwu!");
     }
 
 }
